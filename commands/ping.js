6b282b31630775decbeb08c32f97b3af5dadc0bc
mammahack <serignemamma@gmail.com>
2026-03-08 20:38:56 +0000

    Create ping.js

---
+++ commands/ping.js
@@ -0,0 +1,17 @@
+import stylizedChar from "../utils/fancy.js"
+
+export async function pingTest(client, message) {
+    const remoteJid = message.key.remoteJid
+    const start = Date.now()
+
+    await client.sendMessage(remoteJid, { text: "📡 Pinging..." }, { quoted: message })
+
+    const latency = Date.now() - start
+
+    await client.sendMessage(remoteJid, {
+        text: stylizedChar(
+            `🚀 kaorukoxrt Network\n\n` +
+            `Latency: ${latency} ms\n\n` +
+            `Digital Crew 243`
+        )
+    }, { quoted: message })
